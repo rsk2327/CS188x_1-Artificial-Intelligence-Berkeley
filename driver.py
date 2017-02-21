6195e4dde0d9c69777da28d39a3ef11b589e9fe2
Roshan Santhosh <roshan.santhosh@gmail.com>
2017-02-21 12:35:37 +0530

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,14 +1,16 @@
 import sys
+import resource
+import time
 
-moveList = [{'up':None,'left':None,'right':1,'down':3},
-            {'up':None,'left':0,'right':2,'down':4},
-            {'up':None,'left':1,'right':None,'down':5},
-            {'up':0,'left':None,'right':4,'down':6},
-            {'up':1,'left':3,'right':5,'down':7},
-            {'up':2,'left':4,'right':None,'down':8},
-            {'up':3,'left':None,'right':7,'down':None},
-            {'up':4,'left':6,'right':8,'down':None},
-            {'up':5,'left':7,'right':None,'down':None}]
+moveList = [{'Up':None,'Left':None,'Right':1,'Down':3},
+            {'Up':None,'Left':0,'Right':2,'Down':4},
+            {'Up':None,'Left':1,'Right':None,'Down':5},
+            {'Up':0,'Left':None,'Right':4,'Down':6},
+            {'Up':1,'Left':3,'Right':5,'Down':7},
+            {'Up':2,'Left':4,'Right':None,'Down':8},
+            {'Up':3,'Left':None,'Right':7,'Down':None},
+            {'Up':4,'Left':6,'Right':8,'Down':None},
+            {'Up':5,'Left':7,'Right':None,'Down':None}]
 
 class gameState():
     
@@ -103,7 +105,7 @@ class bfs():
         self.explored = set()
         self.fringe = Queue()
         self.fringe.push(x)
-		
+        
         self.bfsPath = []
         
     def solve(self):
@@ -116,42 +118,102 @@ class bfs():
                 self.max_fringe_size = self.fringe_size
             
             topNode = self.fringe.pop()
+            
             self.fringe_size = self.fringe.length()
-            
             self.search_depth = len(topNode.path)
-            
-            if self.search_depth > self.max_search_depth:
-                self.max_search_depth = self.search_depth
-            
             self.explored.add(topNode.state)
             
-            self.nodes_expanded += 1
+            
             
             if topNode.goalState() == 1:
                 self.bfsPath = topNode.path
                 return topNode.path
+
+            self.nodes_expanded += 1
                 
-            for action in ["up","down","left","right"]:
+            for action in ["Up","Down","Left","Right"]:
                 newState =  topNode.getChildren(action)
                 
                 if newState != None and newState.state not in self.fringe.values and newState.state not in self.explored:
                     self.fringe.push(newState)
                     
-            
+                    if len(newState.path) > self.max_search_depth:
+                        self.max_search_depth = len(newState.path)
+
             sol = self.solve()
             return sol
             
 
+algo = sys.argv[0]
+startingState = "".join([sys.argv[2][i*2] for i in range(9)])
 
-checkState = gameState('125340678')
+checkState = gameState(startingState)
 checkState.path=[]
+start = time.clock()
 
-a = bfs(checkState)
-sol = a.solve()
-print "sol is "
+if algo =="bfs":
+    a = bfs(checkState)
+    sol = a.solve()
+elif algo =="dfs":
+    a = bfs(checkState)
+    sol = a.solve()
+else:
+    a = bfs(checkState)
+    sol = a.solve()
+
+
+end = time.clock()
+
+
+timeTaken = end-start
+
 print a.bfsPath
 print a.nodes_expanded
 print a.fringe_size
 print a.max_fringe_size
 print a.search_depth
 print a.max_search_depth
+print resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1000.0
+
+f = open('output.txt','w')
+
+f.write("path_to_goal: ")
+f.write(str(a.bfsPath))
+f.write("\n")
+
+f.write("cost_of_path: ")
+f.write(str(len(a.bfsPath)))
+f.write("\n")
+
+f.write("nodes_expanded: ")
+f.write(str(a.nodes_expanded))
+f.write("\n")
+
+f.write("fringe_size: ")
+f.write(str(a.fringe_size))
+f.write("\n")
+
+f.write("max_fringe_size: ")
+f.write(str(a.max_fringe_size))
+f.write("\n")
+
+f.write("search_depth: ")
+f.write(str(a.search_depth))
+f.write("\n")
+
+f.write("max_search_depth: ")
+f.write(str(a.max_search_depth))
+f.write("\n")
+
+f.write("running_time: ")
+f.write(str(timeTaken))
+f.write("\n")
+
+f.write("max_ram_usage: ")
+f.write(str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1000.0))
+f.write("\n")
+
+
+f.close()
+
+
